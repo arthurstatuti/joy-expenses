43c7135ff7c3438335c781bd9034da58bc704600
Luiz Casimiro <luizcasimi@gmail.com>
2022-08-15 14:15:17 -0300

    style page Settings.js

--- src/components/ButtonBACK.js
+++ src/components/ButtonBACK.js
@@ -8,9 +8,6 @@ function ButtonBACK() {
     setWhat,
     setWhere,
     setDate,
-    // setDay,
-    // setMonth,
-    // setYear,
     setPrice,
     setCurrency,
     setRate,
@@ -23,9 +20,6 @@ function ButtonBACK() {
     setWhat("");
     setWhere("");
     setDate("");
-    // setDay("");
-    // setMonth("");
-    // setYear("");
     setPrice("");
     setCurrency("");
     setRate(0);
@@ -34,7 +28,7 @@ function ButtonBACK() {
   }
 
   return (
-    <div>
+    <div className=" text-gray-700 bg-gray-100 border border-red-300 rounded py-5 px-5 sm:px-12 sm:text-lg font-bold transform hover:scale-105 shadow-sm hover:shadow-md transition ease-out duration-200">
       <button
         id="button-back"
         type="button"
--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -6,13 +6,14 @@ import ButtonBACK from "../components/ButtonBACK";
 function Settings() {
   return (
     <div>
+
       <Header />
 
-      <h2> SETTINGS </h2>
+      <h2 className=" text-gray-700 bg-gray-100 border-b border-red-300 flex justify-center p-4 font-bold"> SETTINGS </h2>
 
-      <h4> CURRENCIES AVAILABLE </h4>
+      <h4 className=" text-gray-700 flex justify-center p-4 font-bold mt-4"> CURRENCIES AVAILABLE </h4>
 
-      <p>
+      <p className=" text-gray-700 flex justify-center px-4">
         ARS : Argentine Peso <br />
         AUD : Australian Dollar <br />
         BRL : Brazilian Real <br />
@@ -26,13 +27,12 @@ function Settings() {
         USD : American Dollar <br />
       </p>
 
-      <h4> CAREFUL! </h4>
+      <h4 className=" text-red-500 flex justify-center p-4 mt-4 font-bold"> --- CLEAN ALL EXPENSES --- </h4>
 
-      <h4> CLEAN ALL EXPENSES </h4>
-
-      <ButtonCLEAN />
-
-      <ButtonBACK />
+      <div className="flex justify-evenly">
+        <ButtonCLEAN />
+        <ButtonBACK />
+      </div>
 
     </div>
   )
