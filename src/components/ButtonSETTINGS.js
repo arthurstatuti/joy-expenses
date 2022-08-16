513dca20a91a0fd241b1e270580822ad43b3ac41
Luiz Casimiro <luizcasimi@gmail.com>
2022-08-16 00:42:03 -0300

    adjust paddings of ButtonNEW.js and ButtonSETTINGS.js

--- src/components/ButtonSETTINGS.js
+++ src/components/ButtonSETTINGS.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { CogIcon } from '@heroicons/react/solid';
+import { CogIcon } from '@heroicons/react/outline';
 
 function ButtonSETTINGS() {
   const navigate = useNavigate();
@@ -8,12 +8,12 @@ function ButtonSETTINGS() {
   return (
     <div>
       <button
-        className="bg-yellow-400 border border-yellow-800 rounded px-5 py-4 sm:text-lg font-bold transform hover:scale-105 shadow-sm hover:shadow-md transition ease-out duration-200"
+        className="bg-yellow-400 border border-yellow-800 rounded px-6 py-5 sm:text-lg font-bold transform hover:scale-105 shadow-sm hover:shadow-md transition ease-out duration-200"
         id="button-settings"
         type="button"
         onClick={() => navigate('/settings')}
       >
-        <CogIcon className="w-5" />
+        <CogIcon className="w-6" />
       </button>
 
     </div>
